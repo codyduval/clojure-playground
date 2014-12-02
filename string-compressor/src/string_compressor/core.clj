b577af4e978c76898deb6c91e818fa13fcf9d81a
Cody Duval <cody.duval@gmail.com>
2014-12-02 11:42:40 -0500

    String compressor returing hash of char count, need to convert back into string

--- string-compressor/src/string_compressor/core.clj
+++ string-compressor/src/string_compressor/core.clj
@@ -1,7 +1,35 @@
 (ns string-compressor.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn get-string [] 
+  (println "Enter string: ")
+  (let [string-input (read-line)] string-input))
+
+(defn string-to-array [user-string]
+  (seq user-string))
+
+(defn this-character [string-array]
+  (first string-array))
+
+(defn dup-count [seq]
+  (for [[id freq] (frequencies seq)]
+    {id freq}))
+
+(defn split-by-like-chars [user-string]
+  (partition-by identity user-string))
+
+(defn count-chars [string-partition]
+  (dup-count string-partition))
+
+(defn count-by-group [array-of-groups]
+  (map count-chars array-of-groups))
+
+(defn go []
+  (let [user-string (get-string)]
+    (println "You said:" user-string)
+    (println (split-by-like-chars user-string))
+    (println (flatten (map count-chars (split-by-like-chars user-string))))))
+
+(defn -main []
+    (go))
+
