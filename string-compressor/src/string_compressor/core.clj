84822d3ffdfdf817fe76df5c0a020d8d70bdf05f
Cody Duval <cody.duval@gmail.com>
2014-12-02 12:34:20 -0500

    Working program complete.

--- string-compressor/src/string_compressor/core.clj
+++ string-compressor/src/string_compressor/core.clj
@@ -22,13 +22,19 @@
   (dup-count string-partition))
 
 (defn count-by-group [array-of-groups]
-  (map count-chars array-of-groups))
+  (flatten (map count-chars array-of-groups)))
 
+(defn hashes-to-string [list-of-hashes]
+  (apply str (flatten (map concat list-of-hashes))))
+
+(defn optimal-compressed-string [original compressed]
+  (if (< (count original) (count compressed)) original compressed))
+  
 (defn go []
   (let [user-string (get-string)]
     (println "You said:" user-string)
-    (println (split-by-like-chars user-string))
-    (println (flatten (map count-chars (split-by-like-chars user-string))))))
+    (println "Compressed this is:" (hashes-to-string (count-by-group (split-by-like-chars user-string))))
+    (println "The optimal compressed string is:" (optimal-compressed-string (hashes-to-string (count-by-group (split-by-like-chars user-string))) user-string))))
 
 (defn -main []
     (go))
