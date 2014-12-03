987f7dacf3c1fb6340e111528df2ee1057370308
Cody Duval <cody.duval@gmail.com>
2014-12-03 12:23:23 -0500

    Refactor with Mary, add docstrings, and better function naming

--- string-compressor/src/string_compressor/core.clj
+++ string-compressor/src/string_compressor/core.clj
@@ -1,41 +1,54 @@
 (ns string-compressor.core
   (:gen-class))
 
-(defn get-string [] 
+(defn get-string
+  "Gets a user inputted string to be compressed, for example
+  'aaaabbbcccdddeeef'. Returns the user inputted string."
+  [] 
   (println "Enter string: ")
   (let [string-input (read-line)] string-input))
 
-(defn string-to-array [user-string]
-  (seq user-string))
-
-(defn this-character [string-array]
-  (first string-array))
-
-(defn dup-count [seq]
-  (for [[id freq] (frequencies seq)]
-    {id freq}))
-
-(defn split-by-like-chars [user-string]
+(defn split-by-like-chars
+  "Take a user inputted string and split into a list of lists of 
+  similar characters. Eg: ((aaa) (bbb) (ccc) (dd) (ee) (f))." 
+  [user-string]
   (partition-by identity user-string))
 
-(defn count-chars [string-partition]
-  (dup-count string-partition))
+(defn count-chars-by-group
+  "Take a list of character lists and count the number of characters
+  in each list.  Returns a list of maps of each character and its count,
+  eg ({a 3} {b 3} ...)."
+  [list-of-character-groups]
+  (flatten (map (fn [group] {(first group) (count group)})
+                list-of-character-groups)))
 
-(defn count-by-group [array-of-groups]
-  (flatten (map count-chars array-of-groups)))
+(defn hashmaps-of-counts-to-single-string
+  "Take a list of character counts and convert into a single string. 
+  Returns a string like 'a3b3c2d1'"
+  [list-of-hashmaps]
+  (apply str (flatten (map concat list-of-hashmaps))))
 
-(defn hashes-to-string [list-of-hashes]
-  (apply str (flatten (map concat list-of-hashes))))
-
-(defn optimal-compressed-string [original compressed]
+(defn compare-compressed-to-original
+  "Compare the length of a compressed string to the original string and
+  return which ever is shorter."
+  [original compressed]
   (if (< (count original) (count compressed)) original compressed))
-  
-(defn go []
-  (let [user-string (get-string)]
-    (println "You said:" user-string)
-    (println "Compressed this is:" (hashes-to-string (count-by-group (split-by-like-chars user-string))))
-    (println "The optimal compressed string is:" (optimal-compressed-string (hashes-to-string (count-by-group (split-by-like-chars user-string))) user-string))))
 
-(defn -main []
-    (go))
+(defn compress-string
+  "Compresses a user entered string like 'aaaabbccddeeeeff' and returns 
+  a string that replaces repeat characters with counts, eg 'a4b2c2d2e4f2'"
+  [user-string]
+  (-> user-string split-by-like-chars count-chars-by-group hashmaps-of-counts-to-single-string))
+  
+(defn go
+  []
+  (let [user-string (get-string)]
+    (let [compressed-string (compress-string user-string)]
+    (println "You said:" user-string)
+    (println "Compressed this is:" compressed-string)
+    (println "The optimal compressed string is:" (compare-compressed-to-original user-string compressed-string)))))
+
+(defn -main
+  []
+  (go))
 
